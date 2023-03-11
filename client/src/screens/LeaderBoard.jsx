2c6c6bae628195191dbc099ca914154d148d6dbf
Yoav Harel <rupcgroup4@gmail.com>
2023-03-11 18:50:35 +0200

    Update LeaderBoard.jsx

--- client/src/screens/LeaderBoard.jsx
+++ client/src/screens/LeaderBoard.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-// import TabsComponent from "../components/Leaderboard/TabsComponent";
-// import TableComponent from "../components/Leaderboard/TableComponent";
+import TabsComponent from '../components/Leaderboard/TabsComponent';
+import TableComponent from '../components/Leaderboard/TableComponent';
 
 export default function LeaderBoard() {
   return (
     <div>
-      {/* <TabsComponent/>
-    <TableComponent/> */}
+      <TabsComponent />
+      <TableComponent />
     </div>
   );
 }
