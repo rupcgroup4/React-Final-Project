703e5f96b2df518d6f5b4631a923464e99d1f3a6
Yoav Harel <rupcgroup4@gmail.com>
2023-03-24 11:44:49 +0300

    add SQL connection

---
+++ server/server/Models/DAL/SqlConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace RupBNB.Models.DAL
+{
+    public static class SqlConnect
+    {
+        public static SqlConnection Connect()
+        {
+            // read the connection string from the web.config file
+            string connectionString = WebConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+            // create the connection to the db
+            SqlConnection con = new SqlConnection(connectionString);
+
+            con.Open();
+
+            return con;
+
+        }
+    }
+}
